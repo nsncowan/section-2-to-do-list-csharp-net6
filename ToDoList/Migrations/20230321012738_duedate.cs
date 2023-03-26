e1ab7fe724dadbf3303f174ea2ca60154c4e501a
Noah Cowan <nsncowan@gmail.com>
2023-03-26 08:51:04 -0700

    save

---
+++ ToDoList/Migrations/20230321012738_duedate.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    public partial class duedate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DueDate",
+                table: "Items",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "Items");
+        }
+    }
+}
